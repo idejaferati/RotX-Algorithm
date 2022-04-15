1855963fa2589edb0516f1e537bc0164adcbc90e
Ideja Ferati <idejaferati03@gmail.com>
2022-04-15 22:47:03 +0200

    updated form designer code

--- rotX_Windows/Form1.Designer.cs
+++ rotX_Windows/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace rotX_Windows
+﻿namespace rotX_Windows
 {
     partial class Form1
     {
@@ -67,7 +66,83 @@ namespace rotX_Windows
             this.label2.Size = new System.Drawing.Size(121, 29);
             this.label2.TabIndex = 2;
             this.label2.Text = "Plaintext:";
+
+            // button2
+            this.button2.BackColor = System.Drawing.Color.Bisque;
+            this.button2.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Chocolate;
+            this.button2.Location = new System.Drawing.Point(745, 549);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Elephant", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(399, 298);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Key:";
+            
+            // textBox1
+            this.textBox1.BackColor = System.Drawing.Color.BurlyWood;
+            this.textBox1.Location = new System.Drawing.Point(509, 181);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(424, 31);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            
+            // textBox2
+            this.textBox2.BackColor = System.Drawing.Color.BurlyWood;
+            this.textBox2.Location = new System.Drawing.Point(509, 416);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(424, 31);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            
+            // label4
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Elephant", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(326, 416);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ciphertext:";
+            
+            // textBox3
+            this.textBox3.BackColor = System.Drawing.Color.SandyBrown;
+            this.textBox3.Font = new System.Drawing.Font("Bodoni MT", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.ForeColor = System.Drawing.Color.Maroon;
+            this.textBox3.Location = new System.Drawing.Point(193, 65);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox3.Size = new System.Drawing.Size(1022, 41);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "ROT(X) encryption and decryption";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged_1);
+            
+            // textBox4
+            this.textBox4.BackColor = System.Drawing.Color.BurlyWood;
+            this.textBox4.Location = new System.Drawing.Point(509, 298);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(424, 31);
+            this.textBox4.TabIndex = 12;
            
-      }
+        }
     }
 }
+
