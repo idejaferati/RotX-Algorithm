b28336c2ddbe8281a334ccb3f1e649539a52e3f9
Jetë Lajçi <74615773+Jeta52@users.noreply.github.com>
2022-04-15 21:58:02 +0200

    Add files via upload
    Main folder created, with the file called Form1, that contains the first and basic code about the form.

---
+++ rotX_Windows/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rotX_Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int key = Convert.ToInt32(textBox4.Text);
+            string plaintext = textBox1.Text.ToLower();
+            textBox2.Text = rotX.Encrypt(key, plaintext);
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int key = Convert.ToInt32(textBox4.Text);
+            string ciphertext = textBox2.Text.ToLower();
+            textBox1.Text = rotX.Decrypt(key, ciphertext);
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox4.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private static char[] alfabeti = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        
+    }
+}
